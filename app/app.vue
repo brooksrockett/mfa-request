1103fbb1727f4745b2e9d65b81e577917a7d0d75
Brooks Rockett <xylotism@gmail.com>
2026-07-15 23:34:11 -0700

    starting API

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+    woop!
+
+    <pre v-if="error">{{ error }}</pre>
+
+    <pre v-else>{{ JSON.stringify(data, null, 2) }}</pre>
   </div>
 </template>
+
+
+<script setup lang="ts">
+console.log('joop!')
+const { data, error } = await useFetch('/api/helloworld')
+</script>
